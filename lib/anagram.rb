7a891065742c5fdfa138bfe1c4df65c4d1321dc0
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-27 23:44:34 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+ require 'pry'
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(array)
+    array.collect do |potential_anagram|
+      pot_ana_array = potential_anagram.split("").sort 
+
+    end
+
+
+  end
+end
