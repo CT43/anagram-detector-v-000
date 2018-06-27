c1a88390ec3fb3a8edfe87f7bce553d837414e12
Christian Thomas <christian.thomas.2894@gmail.com>
2018-06-27 23:46:31 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,16 +1,17 @@
 # Your code goes here!
  require 'pry'
 class Anagram
-  attr_accessor :anagram
+  attr_accessor :name
 
-  def initialize(anagram)
-    @anagram = anagram
+  def initialize(name)
+    @aname = name
   end
 
   def match(array)
+    name_array = name.split("").sort
     array.collect do |potential_anagram|
-      pot_ana_array = potential_anagram.split("").sort 
-
+      pot_ana_array = potential_anagram.split("").sort
+      if
     end
 
 
